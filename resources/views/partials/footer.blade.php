c7f58f5825deafc5e367c2fb45940ec7f98283e7
Francesco <francesco.savignano96@gmail.com>
2020-12-22 17:51:23 +0100

    footer left impostato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,14 @@
-
+<div class="container">
+    <div class="left">
+        <div class="logo">
+            <img src="{{asset('images/la-molisana-logo.png')}}" alt="La Molisana - logo">
+        </div>
+        <div class="credits">
+            <p>Ragione sociale: La Molisana S.P.A</p>
+            <p>Sede legale: Contrada Colle Api, 100/A</p>
+            <p>Tel: +39 00000000</p>
+            <p>Fax: +39 00000000</p>
+        </div>
+    </div>
+</div>
 <img src="{{asset('images/footer-montagne.jpg')}}" alt="">
