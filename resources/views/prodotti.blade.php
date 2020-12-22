2f27b27cf96042342999d2433438da98a7e4f221
Francesco <francesco.savignano96@gmail.com>
2020-12-22 17:03:09 +0100

    cards grezze stilate dinamicamente

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+
     <h1>Prodotti</h1>
-    @foreach($tipi_di_pasta as $tipo)
-        <p>{{$tipo['titolo']}}</p>
-    @endforeach
+    <div class="cards-container">
+        @foreach($tipi_di_pasta as $tipo)
+            <div class="card">
+                <img src="{{$tipo['src']}}" alt="{{$tipo['titolo']}}">
+                <div class="layer">
+                    <a href="#">{{$tipo['titolo']}}</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
 @endsection
 
 @section('page-title', 'La Molisana - Prodotti')
